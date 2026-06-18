55c0be8dda2dddcbff82ca7c535111deefb8cc7e
Muhammad Maaz Khan <163487911+maazkhan872@users.noreply.github.com>
2026-06-18 22:41:41 +0500

    Add constructor and getters for Subject class

--- src/model/Subject.java
+++ src/model/Subject.java
@@ -5,11 +5,13 @@ public class Subject {
     private String subjectName;
     private int marks;
 
+    // constructor
     public Subject(String subjectName, int marks) {
         this.subjectName = subjectName;
         this.marks = marks;
     }
 
+    // Getter/Setter
     public String getSubjectName() {
         return subjectName;
     }
